0a35ae1fa75cbdeefb331f1695e453270a8ec6fa
Ankit <50232341+imankit007@users.noreply.github.com>
2025-11-10 12:33:18 +0530

    Add Recipe User Service.

---
+++ recipe-user-service/src/main/java/com/recipe/user/service/RecipeUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.recipe.user.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipeUserServiceApplication.class, args);
+    }
+
+}
