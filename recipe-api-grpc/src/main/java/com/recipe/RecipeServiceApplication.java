1540c343ed0a68b3d6dce1aaff3591616ac08123
Ankit <50232341+imankit007@users.noreply.github.com>
2026-01-19 20:34:13 +0530

    Refactor build configuration to streamline dependencies and update Gradle wrapper to version 9.0.0

--- recipe-api-grpc/src/main/java/com/recipe/RecipeServiceApplication.java
+++ recipe-api-grpc/src/main/java/com/recipe/RecipeServiceApplication.java
@@ -2,16 +2,16 @@ package com.recipe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 import java.util.TimeZone;
 
 @SpringBootApplication
-//@ComponentScan(basePackages = "com.recipe")
-//@EnableJpaRepositories(basePackages = "com.recipe.data.jdbc.repository")
-//@EntityScan(basePackages = "com.recipe.data.jdbc.model")
+@ComponentScan(basePackages = "com.recipe")
+@EnableJpaRepositories(basePackages = "com.recipe.data.jdbc.repository")
+@EntityScan(basePackages = "com.recipe.data.jdbc.model")
 public class RecipeServiceApplication {
     public static void main(String[] args) {
         // Ensure the JVM default timezone uses a Postgres-recognized ID (Asia/Kolkata)
