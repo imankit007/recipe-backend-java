e55e239f462751daaefe5c4515f768b2cddb73a5
Ankit <50232341+imankit007@users.noreply.github.com>
2026-02-26 01:42:00 +0530

    Rename settings.gradle to settings.gradle.kts and update project inclusion format

---
+++ settings.gradle.kts
@@ -0,0 +1,13 @@
+rootProject.name = "recipe-backend-java"
+
+include(
+    "recipe-common",
+    "recipe-data",
+    "recipe-api-grpc",
+    "recipe-grpc-proto",
+    "recipe-gateway",
+    "recipe-user-service",
+    "recipe-migration",
+    "recipe-auth",
+    "recipe-api"
+)
